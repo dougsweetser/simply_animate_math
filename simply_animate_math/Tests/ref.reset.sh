c9aeb7433ad0c2703f3d523eda5a53b37254b0be
Douglas Sweetser <dsweetser@blackducksoftare.com>
2015-03-13 07:25:19 -0400

    Updated.

--- simply_animate_math/Tests/ref.reset.sh
+++ simply_animate_math/Tests/ref.reset.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo 'Copies from /tmp/whatever.bmp to ref.whatever.bmp'
-ls ref*bmp | perl -lane '$r = $F[0]; $t = $r; $t =~ s/ref.//; $t = qq(/tmp/$t); print qq(cp $t $r)' | /bin/sh
+ls ref*png | perl -lane '$r = $F[0]; $t = $r; $t =~ s/ref.//; $t = qq(/tmp/$t); print qq(cp $t $r)' | /bin/sh
