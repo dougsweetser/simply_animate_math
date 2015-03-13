3446f0148f7af55a1a8a46eeca1c594d8448911b
Douglas Sweetser <dsweetser@blackducksoftare.com>
2015-03-13 07:26:58 -0400

    Updated

--- simply_animate_math/Tests/simply_animate_math_test.py
+++ simply_animate_math/Tests/simply_animate_math_test.py
@@ -16,56 +16,72 @@ class SimplyAnimateMathTests(unittest.TestCase):
     def test_create_background(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.create_background(template.plane.line)
-        new_img = Image.open("/tmp/background.bmp")
-        ref_img = Image.open("{0}/ref.background.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/background.png")
+        ref_img = Image.open("{0}/ref.background.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
     def test_draw_equation(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.draw_equation("3_plus_3.jpg")
-        new_img = Image.open("/tmp/equation.bmp")
-        ref_img = Image.open("{0}/ref.equation.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/equation.png")
+        ref_img = Image.open("{0}/ref.equation.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
     def test_draw_numbers(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.draw_numbers(("N1n1.jpg", "N1n1.jpg", "N1n1.jpg"))
-        new_img = Image.open("/tmp/numbers.bmp")
-        ref_img = Image.open("{0}/ref.numbers.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/numbers.png")
+        ref_img = Image.open("{0}/ref.numbers.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
     def test_draw_numbers_2d_1(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.draw_numbers_2d_1(("N1n1.jpg", "N1n1.jpg", "N1n1.jpg"))
-        new_img = Image.open("/tmp/numbers_2d_1.bmp")
-        ref_img = Image.open("{0}/ref.numbers_2d_1.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/numbers_2d_1.png")
+        ref_img = Image.open("{0}/ref.numbers_2d_1.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
     def test_draw_numbers_2d_2(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.draw_numbers_2d_2(("N1.jpg", "N1.jpg", "N1.jpg"))
-        new_img = Image.open("/tmp/numbers_2d_2.bmp")
-        ref_img = Image.open("{0}/ref.numbers_2d_2.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/numbers_2d_2.png")
+        ref_img = Image.open("{0}/ref.numbers_2d_2.png".format(self.ref_dir))
+        diff = ImageChops.difference(new_img, ref_img).getbbox()
+        self.assertTrue(diff is None)
+
+    def test_write_time_d1(self):
+        sam = simply_animate_math.SimplyAnimateMath()
+        sam.write_time_d1("2")
+        new_img = Image.open("/tmp/time_d1.png")
+        ref_img = Image.open("{0}/ref.time_d1.png".format(self.ref_dir))
+        diff = ImageChops.difference(new_img, ref_img).getbbox()
+        self.assertTrue(diff is None)
+
+    def test_write_time_d3(self):
+        sam = simply_animate_math.SimplyAnimateMath()
+        sam.write_time_d3("2")
+        new_img = Image.open("/tmp/time_d3.png")
+        ref_img = Image.open("{0}/ref.time_d3.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
     def test_draw_operator(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.draw_operator("plus.jpg")
-        new_img = Image.open("/tmp/operator.bmp")
-        ref_img = Image.open("{0}/ref.operator.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/operator.png")
+        ref_img = Image.open("{0}/ref.operator.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
     def test_draw_equal(self):
         sam = simply_animate_math.SimplyAnimateMath()
         sam.draw_equal("equal.jpg")
-        new_img = Image.open("/tmp/equal.bmp")
-        ref_img = Image.open("{0}/ref.equal.bmp".format(self.ref_dir))
+        new_img = Image.open("/tmp/equal.png")
+        ref_img = Image.open("{0}/ref.equal.png".format(self.ref_dir))
         diff = ImageChops.difference(new_img, ref_img).getbbox()
         self.assertTrue(diff is None)
 
