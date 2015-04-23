8bdde848e8b1a231a98cd1d6260028c935337f06
Doug Sweetser <sweetser@alum.mit.edu>
2015-04-22 20:41:39 -0400

    Added smoshed time to qft picture.

--- simply_animate_math/simply_animate_math.py
+++ simply_animate_math/simply_animate_math.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """
-usage: simply_animate_math.py [-h]
+usage: simply_animate_math.py [-s THING...] [-h]
 
 Pulls images together to make an animation
 
 optional arguments:
+  -s, --skip THING...  skip 'plane' or 'd1' or 'd3' [default: None]
   -k, --keep  keep the png files when done [default: False]
   -h, --help  show this help message and exit
 """
@@ -24,7 +25,8 @@ class SimplyAnimateMath:
     A tool to take photos and make animations.
     """
 
-    def __init__(self):
+    def __init__(self, skip):
+        self.skip = skip
         self.base_img = Image.new('RGBA', (config.width, config.height), \
                 (183, 189, 177, 256))
         self.photos = photos.get_photos()
@@ -35,26 +37,35 @@ class SimplyAnimateMath:
     def run(self):
         "Runs all."
 
-        self.animate_plane(config.pure_numbers, "pure")
-        self.animate_plane(config.qft_numbers, "qft")
+        if 'plane' not in self.skip:
+            self.animate_plane(config.pure_numbers, "pure")
+            self.animate_plane(config.qft_numbers, "qft", smosh_time=True)
 
         constants = ("t", "r", "m")
+        common_names = []
 
         for constant in constants:
-            self.animate_plane(\
-                    config.d2_constant_numbers[constant], \
-                    "plane_{c}".format(c=constant))
-            self.animate_plane_d1(\
-                    config.d2_constant_numbers[constant], \
-                    config.d1[constant]["numbers"], \
-                    "dynamic_d1_{c}".format(c=constant))
-            self.animate_plane_d1_d3(\
-                    config.d2_constant_numbers[constant], \
-                    config.d1[constant]["numbers"], \
-                    config.d3[constant]["numbers"], \
-                    "dynamic_d3_{c}".format(c=constant))
+            if 'plane' not in self.skip:
+                self.animate_plane(\
+                        config.d2_constant_numbers[constant], \
+                        "plane_{c}".format(c=constant))
+ 
+            if 'd1' not in self.skip:    
+                self.animate_plane_d1(\
+                        config.d2_constant_numbers[constant], \
+                        config.d1[constant]["numbers"], \
+                        "dynamic_d1_{c}".format(c=constant))
+                common_names.append("dynamic_d1")
 
-            for common_name in ("dynamic_d1", "dynamic_d3"):
+            if 'd3' not in self.skip:    
+                self.animate_plane_d1_d3(\
+                        config.d2_constant_numbers[constant], \
+                        config.d1[constant]["numbers"], \
+                        config.d3[constant]["numbers"], \
+                        "dynamic_d3_{c}".format(c=constant))
+                common_names.append("dynamic_d3")
+
+            for common_name in common_names:
                 merge_2_gifs(common_name, constant, "plus", "times")
                 merge_2_gifs(common_name, constant, "minus", "div")
                 merge_box_gifs(common_name, constant, "plus_times", "minus_div")
@@ -138,25 +149,31 @@ class SimplyAnimateMath:
         self.work_img.save("/tmp/numbers_2d_3.png")
 
 
-    def write_time_d1(self, time_string):
+    def write_time(self, time):
         """Writes time as a number."""
 
+        time_string = str(time)
         draw = ImageDraw.Draw(self.work_img)
-        draw.text(template.time_1d_pos, time_string, font=self.font, \
-                fill=(0, 0, 0, 255))
+
+        for t_pos in template.time_pos.values():
+            draw.text(t_pos, time_string, font=self.font, fill=(0, 0, 0, 255))
+
         del draw
 
-        self.work_img.save("/tmp/time_d1.png")
+        self.work_img.save("/tmp/time_123.png")
 
-    def write_time_d3(self, time_string):
-        """Writes time as a number."""
+    def write_smosh_time(self):
+        """Writes all time smoshed together."""
 
         draw = ImageDraw.Draw(self.work_img)
-        draw.text(template.time_3d_pos, time_string, font=self.font, \
-                fill=(0, 0, 0, 255))
+
+        for n in range(1, 7):
+            for t_pos in template.time_pos.values():
+                draw.text((t_pos[0] + 5 * n - 20,t_pos[1]), str(n), font=self.font, fill=(0, 0, 0, 255))
+
         del draw
 
-        self.work_img.save("/tmp/time_d3.png")
+        self.work_img.save("/tmp/time_123.png")
 
 
     def draw_operator(self, operator_file):
@@ -182,7 +199,7 @@ class SimplyAnimateMath:
 
 
     def animate_plane(self, d2_numbers, short_name="test", \
-            dir_name="Animations"):
+            dir_name="Animations", smosh_time=False):
         """Given 2D numbers triplets, shows plus, minus, times, and div animation."""
 
         for count, equation, operator, d2_number in \
@@ -195,12 +212,15 @@ class SimplyAnimateMath:
             self.draw_equal(config.equal)
             self.draw_numbers(d2_number)
 
+            if smosh_time:
+                self.write_smosh_time()
+
             self.work_img.save("{dn}/{sn}.{ct}.png".format(\
                     dn=dir_name, sn=short_name, ct=count))
 
-            os.system("{convert} -loop 0 -delay 350 {dn}/{sn}.*.png {dn}/{sn}.gif".format(\
-                    convert=config.convert, dn=dir_name, sn=short_name))
-        print("gif done: {dn}/{sn}.gif".format(dn=dir_name, sn=short_name))
+            os.system("{convert} -loop 0 -delay 350 {dn}/{sn}.*.png {dn}/{sn}_{w}.gif".format(\
+                    convert=config.convert, dn=dir_name, sn=short_name, w=config.width))
+        print("gif done: {dn}/{sn}_{w}.gif".format(dn=dir_name, sn=short_name, w=config.width))
 
 
     def animate_plane_d1(self, d2_numbers, d1_number_numbers, \
@@ -218,18 +238,18 @@ class SimplyAnimateMath:
                 self.draw_equal(config.equal)
                 self.draw_numbers_2d_1(d2_number)
                 self.draw_numbers_2d_2(d1_number)
-                self.write_time_d1(str(count + 1))
+                self.write_time(count + 1)
 
                 op_name = re.sub(r'....$', r'', operator)
                 self.work_img.save("{dn}/{sn}_{on}.{ct}.png".format(\
                         dn=dir_name, sn=short_name, on=op_name, \
                         ct=str(100 + count)))
 
-            os.system("{convert} -loop 0 -delay 60 {dn}/{sn}_{on}.*.png {dn}/{sn}_{on}.gif".format(\
+            os.system("{convert} -loop 0 -delay 60 {dn}/{sn}_{on}.*.png {dn}/{sn}_{on}_{w}.gif".format(\
                     convert=config.convert, dn=dir_name, \
-                    sn=short_name, on=op_name))
-            print("gif done: {dn}/{sn}_{on}.gif".format(\
-                    dn=dir_name, sn=short_name, on=op_name))
+                    sn=short_name, on=op_name, w=config.width))
+            print("gif done: {dn}/{sn}_{on}_{w}.gif".format(\
+                    dn=dir_name, sn=short_name, on=op_name, w=config.width))
 
 
     def animate_plane_d1_d3(self, d2_numbers, d1_number_numbers, \
@@ -252,29 +272,28 @@ class SimplyAnimateMath:
                 self.draw_numbers_2d_1(d2_number)
                 self.draw_numbers_2d_2(d1_number)
                 self.draw_numbers_2d_3(d3_number)
-                self.write_time_d1(str(count + 1))
-                self.write_time_d3(str(count + 1))
+                self.write_time(count + 1)
 
                 op_name = re.sub(r'....$', r'', operator)
                 self.work_img.save("{dn}/{sn}_{on}.{ct}.png".format(\
                     dn=dir_name, sn=short_name, on=op_name, ct=count + 100))
 
-            os.system("{convert} -loop 0 -delay 60 {dn}/{sn}_{on}.*.png {dn}/{sn}_{on}.gif".format(\
+            os.system("{convert} -loop 0 -delay 60 {dn}/{sn}_{on}.*.png {dn}/{sn}_{on}_{w}.gif".format(\
                     convert=config.convert, dn=dir_name, sn=short_name, \
-                    on=op_name))
-            print("gif done: {dn}/{sn}_{on}.gif".format(dn=dir_name, sn=short_name, on=op_name))
+                    on=op_name, w=config.width))
+            print("gif done: {dn}/{sn}_{on}_{w}.gif".format(dn=dir_name, sn=short_name, on=op_name, w=config.width))
 
 
 def merge_2_gifs(common_name, constant, operator_1, operator_2, dir_name="Animations"):
     """Merge two animations animations."""
 
-    file_1 = "{dn}/{cn}_{con}_{op_1}.gif".format(\
-            dn=dir_name, cn=common_name, con=constant, op_1=operator_1)
-    file_2 = "{dn}/{cn}_{con}_{op_2}.gif".format(\
-            dn=dir_name, cn=common_name, con=constant, op_2=operator_2)
-    file_1_2 = "{dn}/{cn}_{con}_{op_1}_{op_2}.gif".format(\
+    file_1 = "{dn}/{cn}_{con}_{op_1}_{w}.gif".format(\
+            dn=dir_name, cn=common_name, con=constant, op_1=operator_1, w=config.width)
+    file_2 = "{dn}/{cn}_{con}_{op_2}_{w}.gif".format(\
+            dn=dir_name, cn=common_name, con=constant, op_2=operator_2, w=config.width)
+    file_1_2 = "{dn}/{cn}_{con}_{op_1}_{op_2}_{w}.gif".format(\
             dn=dir_name, cn=common_name, con=constant, \
-            op_1=operator_1, op_2=operator_2)
+            op_1=operator_1, op_2=operator_2, w=config.width)
 
     command = r"{convert} {f_1} -repage {w2}x{h} -coalesce null: \( {f_2} -coalesce \) -geometry +{w}+0 -layers Composite {f_1_2}".format(\
             convert=config.convert, f_1=file_1, f_2=file_2, \
@@ -289,13 +308,13 @@ def merge_box_gifs(common_name, constant, operator_1, operator_2, \
     dir_name="Animations"):
     """Merge two animations animations."""
 
-    file_1 = "{dn}/{cn}_{con}_{op_1}.gif".format(\
-            dn=dir_name, cn=common_name, con=constant, op_1=operator_1)
-    file_2 = "{dn}/{cn}_{con}_{op_2}.gif".format(\
-            dn=dir_name, cn=common_name, con=constant, op_2=operator_2)
-    file_1_2 = "{dn}/{cn}_{con}_{op_1}_{op_2}.gif".format(\
+    file_1 = "{dn}/{cn}_{con}_{op_1}_{w}.gif".format(\
+            dn=dir_name, cn=common_name, con=constant, op_1=operator_1, w=config.width)
+    file_2 = "{dn}/{cn}_{con}_{op_2}_{w}.gif".format(\
+            dn=dir_name, cn=common_name, con=constant, op_2=operator_2, w=config.width)
+    file_1_2 = "{dn}/{cn}_{con}_{op_1}_{op_2}_{w}.gif".format(\
             dn=dir_name, cn=common_name, con=constant, \
-            op_1=operator_1, op_2=operator_2)
+            op_1=operator_1, op_2=operator_2, w=config.width)
 
     os.system(r"{convert} {f_1} -repage {w2}x{h2} -coalesce null: \( {f_2} -coalesce \) -geometry +0+{h} -layers Composite {f_1_2}".format(\
             convert=config.convert, f_1=file_1, f_2=file_2, \
@@ -308,7 +327,7 @@ if __name__ == "__main__":
 
     ARGS = docopt(__doc__)
 
-    SAM = SimplyAnimateMath()
+    SAM = SimplyAnimateMath(skip=ARGS['--skip'])
     SAM.run()
 
     if "--keep" not in ARGS:
